b080053a8ca870f4434d2a293445450a1c04125c
hafizmahad <awaisraxaa1@gmail.com>
2023-08-21 00:39:05 -0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
 
 
         ),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const SplashScreen()
